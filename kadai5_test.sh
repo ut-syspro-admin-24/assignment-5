78875655f9e4c9b1602638607d4282959f1bf2e8
momo-trip <73386097+momo-trip@users.noreply.github.com>
2024-05-03 05:33:49 +0900

    Update kadai5_test.sh

--- kadai5_test.sh
+++ kadai5_test.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-# Test code for syspro2023 kadai5
+# Test code for syspro2024 kadai5
 # Written by Shinichi Awamoto and Daichi Morita
-# Edited by PENG AO and Joe Hattori
+# Edited by Momoko Shiraishi
 
 state=0
 warn() { echo $1; state=1; }
 dir=$(mktemp -d)
 trap "rm -rf $dir" 0
-
-check-report() {
-    if [ ! -f report-$1.txt ]; then
-        $2 "kadai-$1: Missing report-$1.txt."
-    elif [ `cat report-$1.txt | wc -l` -eq 0 ]; then
-        $2 "kadai-$1: 'report-$1.txt' is empty!"
-    fi
-}
 
 kadai-a() {
     if [ -d kadai-a ]; then
@@ -60,8 +52,6 @@ kadai-a() {
         if [ `grep '\-Wall' Makefile | wc -l` -eq 0 ]; then
             warn "kadai-a: Missing '-Wall' option."
         fi
-
-        check-report a warn
 
         popd > /dev/null 2>&1
     else
@@ -115,8 +105,6 @@ kadai-b() {
             warn "kadai-b: Missing '-Wall' option."
         fi
 
-        check-report b warn
-
         popd > /dev/null 2>&1
     else
         warn "kadai-b: No 'kadai-bcde' directory"
@@ -167,8 +155,6 @@ kadai-c() {
         if [ `grep '\-Wall' Makefile | wc -l` -eq 0 ]; then
             warn "kadai-c: Missing '-Wall' option."
         fi
-
-        check-report c warn
 
         popd > /dev/null 2>&1
     else
@@ -246,8 +232,6 @@ kadai-d() {
             warn "kadai-d: Missing '-Wall' option."
         fi
 
-        check-report d warn
-
         popd > /dev/null 2>&1
     fi
 }
@@ -273,8 +257,6 @@ kadai-e() {
     if [ -d kadai-bcde ] && [ -f kadai-bcde/report-e.txt ]; then
         cp -r kadai-bcde $dir
         pushd $dir/kadai-bcde > /dev/null 2>&1
-
-        check-report e warn
 
         popd > /dev/null 2>&1
     fi
